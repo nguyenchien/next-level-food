9549852ad8e3adb478e05690c3be73b4637a2e40
Chien Nguyen <chiennguyen1702@gmail.com>
2024-08-17 09:23:57 +0700

    meal not found

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -1,9 +1,13 @@
 import Image from 'next/image';
+import { notFound } from 'next/navigation';
 import classes from './page.module.css';
 import { getMeal } from '@/lib/meals';
 
 export default function MealDetailPage({params}) {
   const meal = getMeal(params.mealSlug);
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, '<br/ >');
 
   return (
