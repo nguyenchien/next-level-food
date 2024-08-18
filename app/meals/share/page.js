f83eddbbdde3720e36cba066e18364db8e6f5550
Chien Nguyen <chiennguyen1702@gmail.com>
2024-08-18 22:22:19 +0700

    useFormStatus

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,4 +1,5 @@
 import ImagePicker from '@/components/meals/image-picker';
+import FormSubmitButton from '@/components/meals/form-submit-button';
 import { ShareMeal } from '@/lib/actions';
 import classes from './page.module.css';
 
@@ -43,7 +44,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <FormSubmitButton />
           </p>
         </form>
       </main>
